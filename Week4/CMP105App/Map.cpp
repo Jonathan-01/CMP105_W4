2adae7cc153c999385199d4dc2e6fffc81f3bda0
JonathanTee <1900172@uad.ac.uk>
2020-02-27 10:50:57 +0000

    The map now scrolls

--- Week4/CMP105App/Map.cpp
+++ Week4/CMP105App/Map.cpp
@@ -1,8 +1,8 @@
 #include "Map.h"
 Map::Map(sf::RenderWindow* hwnd) {
-	viewX = 300.f;
+	viewX = 600.f;
 	window = hwnd;
-	sf::View view(sf::Vector2f(500, 338), sf::Vector2f(300.f, 200.f));
+	sf::View view(sf::Vector2f(250, 338), sf::Vector2f(1000.f, 600.f));
 }
 
 Map::~Map() {
@@ -12,14 +12,14 @@ Map::~Map() {
 void Map::scrollMap(float dt) {
 
 	if (input->isKeyDown(sf::Keyboard::A)) {
-		viewX += 50;
+		viewX -= 50*dt;
 	}
 	else if (input->isKeyDown(sf::Keyboard::D)) {
-		viewX -= 50;
+		viewX += 50*dt;
 	}
 }
 
 void Map::update() {
-	sf::View view(sf::Vector2f(viewX, 338), sf::Vector2f(300.f, 200.f));
+	sf::View view(sf::Vector2f(viewX, 338), sf::Vector2f(1200.f, 675.f));
 	window->setView(view);
 }
--- Week4/CMP105App/Player.cpp
+++ Week4/CMP105App/Player.cpp
@@ -9,15 +9,15 @@ Player::~Player() {
 
 void Player::handleInput(float dt) {
 	if (input->isKeyDown(sf::Keyboard::W)) {
-		move(0,-50 *dt);
-	}
-	else if (input->isKeyDown(sf::Keyboard::A)) {
-		move(-50 *dt,0);
+		move(0,-60 *dt);
 	}
 	else if (input->isKeyDown(sf::Keyboard::S)) {
-		move(0, 50 *dt);
+		move(0, 60 *dt);
+	}
+	if (input->isKeyDown(sf::Keyboard::A)) {
+		move(-60 * dt, 0);
 	}
 	else if (input->isKeyDown(sf::Keyboard::D)) {
-		move(50 *dt, 0);
+		move(60 *dt, 0);
 	}
 }
